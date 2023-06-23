9f0a890c8f51402cd22c07285d0a27ec311f3191
big-choi <bos33210@naver.com>
2023-06-24 00:57:50 +0900

    array, object state 변경

--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
+/* eslint-disable */
 import './App.css';
 import { useState } from 'react';
 
 function App() {
-  let post = '강남 우동 맛집';
-  let [글제목, b] = useState('남자 코트 추천');
+  let [글제목, 글제목변경] = useState(['남자코트 추천', '강남 우동맛집', '파이썬독학']);
+  let [따봉, 따봉변경] = useState(0);
+
+  const 함수 = () => {
+    따봉변경(따봉 + 1);
+  };
+
+  const 제목변경 = () => {
+    let copy = [...글제목];
+    copy[0] = '여자코트 추천';
+    글제목변경(copy);
+  }
+
+  const 제목정렬 = () => {
+    let copy = [...글제목].sort();
+    글제목변경(copy);
+  }
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4>블로그임</h4>
+        <h4>ReactBlog</h4>
+        <button onClick={제목변경}>첫글바꾸는버튼</button>
+        <button onClick={제목정렬}>가나다순 정렬</button>
       </div>
       <div className="list">
-        <h4>글제목</h4>
+        <h4>
+          {글제목[0]} <span onClick={함수}>👍</span> {따봉}
+        </h4>
         <p>2월 17일 발행</p>
       </div>
-      <h4>{post}</h4>
+      <div className="list">
+        <h4>{글제목[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
